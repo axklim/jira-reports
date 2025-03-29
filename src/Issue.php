e515da51d6650f081e599fb3b85aa5666d65036f
Aleksey Klimov <ax.klim@gmail.com>
2025-03-29 02:31:12 +0100

    added Issue class

--- src/Issue.php
+++ src/Issue.php
@@ -4,5 +4,21 @@ namespace JiraReport;
 
 class Issue
 {
+    public function __construct(
+        public readonly string $key,
+        private readonly string $summary,
+        private readonly string $status,
+        private readonly int $storyPoints,
+    ) {
+    }
 
+    public static function make($payload): self
+    {
+        return new self(
+            $payload['key'],
+            $payload['fields']['summary'],
+            $payload['fields']['status']['name'],
+            (int) $payload['fields']['customfield_10406'],
+        );
+    }
 }
